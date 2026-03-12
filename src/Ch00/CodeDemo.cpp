eeecedb2db81983bee2192a07108e71ee6c73c7d
rmosquera1978-netizen <rmosquera1978@gmail.com>
2026-03-12 14:50:27 +0000

    add Commit

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 whit modifications
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
@@ -8,7 +8,7 @@ int main(){
     float num_1, num_2, result;
 
     std::cout << "Enter number 1: " << std::flush;
-    std::cin >> num_1;
+    std::cin >> num_1; 
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
